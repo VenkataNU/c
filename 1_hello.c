d0c0dd5d0fdfe49b37ac6e97bd63bb2dd93c43cd
UVN <naidu1111@gmail.com>
2017-06-14 06:56:24 -0500

    Create 1_hello.c

---
+++ 1_hello.c
@@ -0,0 +1,17 @@
+/*gcc - gnu c compiler can be used to compile the program
+   ex., "gcc hello.c" will compile the c program
+Once you compile the program, "a.out" is created in the same folder and 
+   you can execute the program by running "a.out"
+The main parts of the program are functions and variables.
+Functions will specify the work to be done and variables hold the values that are used in those functions
+*/
+
+/*Including the standard input output header file */
+#include <stdio.h>
+/*we can name your function with any name but every c program should have a main function
+  as c program will start executing from main function*/
+main(){
+/* we can have n number of lines in the body of the function, but we have a single line here */
+/* In the below line we are calling a built-in provided function printf and passing the argument list to it */
+printf("Jai Jai Ganesha!!!\n");
+}
